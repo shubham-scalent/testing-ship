ff85d7b9186754336d423a20813dcc1ec78a5bbc
shubham <ahire.shubham12@gmail.com>
2024-02-13 18:48:13 +0530

    temp

--- pkg/shiprocket.go
+++ pkg/shiprocket.go
@@ -1,8 +1,13 @@
 package pkg
 
-import "github.com/shubham-scalent/testing-ship/apimodel"
+import (
+	"net/http"
+
+	"github.com/shubham-scalent/testing-ship/apimodel"
+)
 
 type ShiprockertService interface {
 	GetToken(config apimodel.Config) (string, error)
 	GetAvailableCouriers(request apimodel.CourierAvailabityRequest, config apimodel.Config, token string) (*apimodel.CourierAvailabityResponse, error)
+	GetAvailableCouriers2(request apimodel.CourierAvailabityRequest, config apimodel.Config, token string) (*http.Response, error)
 }
