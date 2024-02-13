403870b635c379fadd42f79b48b63b3a315a5903
shubham <ahire.shubham12@gmail.com>
2024-02-13 19:08:23 +0530

    temp

--- apimodel/request.go
+++ apimodel/request.go
@@ -7,8 +7,8 @@ type Config struct {
 }
 
 type CourierAvailabityRequest struct {
-	PickupPostcode   int     `json:"pickup_Postcode" binding:"omitempty"`
-	DeliveryPostcode int     `json:"delivery_Postcode" binding:"omitempty"`
+	PickupPostcode   int     `json:"pickup_postcode" binding:"omitempty"`
+	DeliveryPostcode int     `json:"delivery_postcode" binding:"omitempty"`
 	OrderID          int     `json:"order_id" binding:"omitempty"`
 	Cod              int     `json:"cod" binding:"omitempty"`
 	Weight           float64 `json:"weight" binding:"omitempty"`
--- pkg/shiprocket.go
+++ pkg/shiprocket.go
@@ -1,13 +1,10 @@
 package pkg
 
 import (
-	"net/http"
-
 	"github.com/shubham-scalent/testing-ship/apimodel"
 )
 
 type ShiprockertService interface {
 	GetToken(config apimodel.Config) (string, error)
 	GetAvailableCouriers(request apimodel.CourierAvailabityRequest, config apimodel.Config, token string) (*apimodel.CourierAvailabityResponse, error)
-	GetAvailableCouriers2(request apimodel.CourierAvailabityRequest, config apimodel.Config, token string) (*http.Response, error)
 }
