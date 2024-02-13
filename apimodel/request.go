22ce4bf08849a27ccd0eda6d6ec58da7cfc6befe
shubham <ahire.shubham12@gmail.com>
2024-02-13 19:02:01 +0530

    temp

--- apimodel/request.go
+++ apimodel/request.go
@@ -9,7 +9,7 @@ type Config struct {
 type CourierAvailabityRequest struct {
 	PickupPostcode   int     `json:"pickup_Postcode" binding:"omitempty"`
 	DeliveryPostcode int     `json:"delivery_Postcode" binding:"omitempty"`
-	OrderID          int     `json:"order_id"`
+	OrderID          int     `json:"order_id" binding:"omitempty"`
 	Cod              int     `json:"cod" binding:"omitempty"`
 	Weight           float64 `json:"weight" binding:"omitempty"`
 }
