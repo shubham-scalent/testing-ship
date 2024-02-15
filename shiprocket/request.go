c164f5fa654aa0676753365611ed1122a8fe6a4f
shubham <ahire.shubham12@gmail.com>
2024-02-15 10:36:17 +0530

    temp

--- shiprocket/request.go
+++ shiprocket/request.go
@@ -1,9 +1,9 @@
 package shiprocket
 
 type CourierAvailabityRequest struct {
-	PickupPostcode   int     `json:"pickup_postcode" validate:"omitempty"`
-	DeliveryPostcode int     `json:"delivery_postcode" validate:"omitempty"`
-	OrderID          int     `json:"order_id" validate:"omitempty"`
-	Cod              int     `json:"cod" validate:"omitempty"`
-	Weight           float64 `json:"weight" validate:"omitempty"`
+	PickupPostcode   int     `json:"pickup_postcode"`
+	DeliveryPostcode int     `json:"delivery_postcode"`
+	OrderID          *int    `json:"order_id"`
+	Cod              int     `json:"cod"`
+	Weight           float64 `json:"weight"`
 }
