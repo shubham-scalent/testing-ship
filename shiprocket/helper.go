a271a625963d996649218e352e0cf059b59f6231
shubham <ahire.shubham12@gmail.com>
2024-02-20 11:12:22 +0530

    temp

--- shiprocket/helper.go
+++ shiprocket/helper.go
@@ -3,53 +3,11 @@ package shiprocket
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
 	"io"
 	"net/http"
 )
 
-func SendRequest(method, path string, BaseURL string, Token string, body interface{}) (*http.Response, error) {
-	jsonData, err := json.Marshal(body)
-	if err != nil {
-		return nil, err
-	}
-
-	req, err := http.NewRequest(method, BaseURL+path, bytes.NewBuffer(jsonData))
-	if err != nil {
-		return nil, err
-	}
-
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Authorization", "Bearer "+Token)
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return nil, err
-	}
-
-	return resp, err
-}
-
-func ReadResponse(resp *http.Response, result interface{}) error {
-	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("bad status code: %d", resp.StatusCode)
-	}
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return err
-	}
-
-	err = json.Unmarshal(body, result)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func SendRequest2(method, path string, BaseURL string, Token string, requestBody interface{}, responseBody interface{}) *ErrorResponse {
+func SendRequest(method, path string, BaseURL string, Token string, requestBody interface{}, responseBody interface{}) *ErrorResponse {
 
 	var errResp ErrorResponse
 
@@ -71,6 +29,7 @@ func SendRequest2(method, path string, BaseURL string, Token string, requestBody
 	if err != nil {
 		return &errResp
 	}
+	defer resp.Body.Close()
 
 	respBody, err := io.ReadAll(resp.Body)
 	if err != nil {
@@ -93,3 +52,44 @@ func SendRequest2(method, path string, BaseURL string, Token string, requestBody
 
 	return nil
 }
+
+// func SendRequest(method, path string, BaseURL string, Token string, body interface{}) (*http.Response, error) {
+// 	jsonData, err := json.Marshal(body)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+
+// 	req, err := http.NewRequest(method, BaseURL+path, bytes.NewBuffer(jsonData))
+// 	if err != nil {
+// 		return nil, err
+// 	}
+
+// 	req.Header.Set("Content-Type", "application/json")
+// 	req.Header.Set("Authorization", "Bearer "+Token)
+
+// 	client := &http.Client{}
+// 	resp, err := client.Do(req)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+
+// 	return resp, err
+// }
+
+// func ReadResponse(resp *http.Response, result interface{}) error {
+// 	if resp.StatusCode != http.StatusOK {
+// 		return fmt.Errorf("bad status code: %d", resp.StatusCode)
+// 	}
+
+// 	body, err := io.ReadAll(resp.Body)
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	err = json.Unmarshal(body, result)
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	return nil
+// }
--- shiprocket/shiprocketImpl.go
+++ shiprocket/shiprocketImpl.go
@@ -1,16 +1,9 @@
 package shiprocket
 
-func (s *ShiprocketClient) GetAvailableCouriers(request CourierAvailabityRequest) (*CourierAvailabityResponse, error) {
-	// Create a new request
-	resp, err := SendRequest("GET", "/v1/external/courier/serviceability/", s.Config.BaseURL, s.TokenConfig.Token, request)
-	if err != nil {
-		return nil, err
-	}
-
-	defer resp.Body.Close()
+func (s *ShiprocketClient) GetAvailableCouriers(request CourierAvailabityRequest) (*CourierAvailabityResponse, *ErrorResponse) {
 
 	var response CourierAvailabityResponse
-	err = ReadResponse(resp, &response)
+	err := SendRequest("GET", "/v1/external/courier/serviceability/", s.Config.BaseURL, s.TokenConfig.Token, request, &response)
 	if err != nil {
 		return nil, err
 	}
@@ -21,8 +14,7 @@ func (s *ShiprocketClient) GetAvailableCouriers(request CourierAvailabityRequest
 func (s *ShiprocketClient) CreateOrder(request CreateOrderRequest) (*CreateOrderResponse, *ErrorResponse) {
 
 	var response CreateOrderResponse
-
-	errResp := SendRequest2("POST", "/v1/external/orders/create/adhoc", s.Config.BaseURL, s.TokenConfig.Token, request, &response)
+	errResp := SendRequest("POST", "/v1/external/orders/create/adhoc", s.Config.BaseURL, s.TokenConfig.Token, request, &response)
 	if errResp != nil {
 		return nil, errResp
 	}
