28a5b3afd04c3d41f6feb50e34ab57d58c45be93
shubham <ahire.shubham12@gmail.com>
2024-05-08 18:35:03 +0530

    temp

--- pincode.go
+++ pincode.go
@@ -2,7 +2,7 @@ package shiprocket
 
 func (s *ShiprocketClient) GetLocalityDetails(request GetLocalityDetailsRequest) (*GetLocalityDetailsResponse, *ErrorResponse) {
 	var response GetLocalityDetailsResponse
-	errResp := SendRequest("GET", "open/postcode/details", BASE_URL, s.tokenConfig.Token, request, &response)
+	errResp := SendRequest("GET", "/v1/external/open/postcode/details", BASE_URL, s.tokenConfig.Token, request, &response)
 	if errResp != nil {
 		return nil, errResp
 	}
